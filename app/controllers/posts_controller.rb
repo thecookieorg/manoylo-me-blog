7a35a65f493aae1e39ac8d053eda74e76861d59e
Marko Manojlovic <marko@thecookieapps.com>
2016-02-23 20:39:24 +0000

    modified views for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     #@posts = Post.all
-    @posts = Post.paginate(:page => params[:page], :per_page => 3)
+    @posts = Post.paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /posts/1
