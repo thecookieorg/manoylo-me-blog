3a5728849ab9ebe1eb5f9ab7fd3d07d1f75c84c1
Marko Manojlovic <marko@thecookieapps.com>
2016-02-23 05:46:51 +0000

    Limited post count on home page to 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.all.order('created_at DESC').limit(3)
   end
 
   def about
