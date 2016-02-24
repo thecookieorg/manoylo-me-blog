3b243620cef020911901099883ea938e3fa77342
Marko Manojlovic <marko@thecookieapps.com>
2016-02-24 17:08:24 +0000

    modified contact me page

---
+++ app/models/contact.rb
@@ -0,0 +1,15 @@
+class Contact < MailForm::Base
+    attributes :name,       :validate => true
+    attributes :email,      :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+    attributes :message,    :validate => true
+    attributes :nickname,   :captcha => true
+    
+    def headers 
+        {
+            :subject => "Contact Form ",
+            :to => "marko@thecookiedining.com",
+            :from => %("#{name}" <#{email}>)
+        }
+    end
+        
+end
